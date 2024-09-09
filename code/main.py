cb9ccbf381c54d8310f8dda330be5fe79a8e65b4
bilalalemayhu <116437035+bilalalemayhu@users.noreply.github.com>
2024-09-09 13:27:53 +0200

    hha

--- code/main.py
+++ code/main.py
@@ -1,1 +1,18 @@
 import pygame
+
+pygame.init()
+
+width = 800
+height = 400
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+pygame.display.set_caption('Suika')
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+
+    pygame.display.flip()
