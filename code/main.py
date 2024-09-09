41b65d46a005bf77a61f1e9682cbb5bd4d42f780
Normallyyy <normallyyparkour@gmail.com>
2024-09-09 13:43:35 +0200

    fff

--- code/main.py
+++ code/main.py
@@ -8,7 +8,7 @@ screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 pygame.display.set_caption('Suika')
 
-
+k = "hh"
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
