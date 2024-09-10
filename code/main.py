1c84677d9ece12ec81494094d9bd60e038aecacd
Normallyyy <normallyyparkour@gmail.com>
2024-09-10 11:28:46 +0200

    comments

--- code/main.py
+++ code/main.py
@@ -2,13 +2,14 @@ import pygame
 
 pygame.init()
 
+# Definitions
 width = 800
 height = 400
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 pygame.display.set_caption('Suika Game')
 
-
+# Game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
